9fcb6962f8fa0abb40016a13de6edf19b78a9b6e
Dragneel0112 <pillayruven@gmail.com>
2023-05-22 10:38:46 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Requests from API
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    u_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/'.format(u_id)
+    todos_url = url + 'todos'
+    user = requests.get(url).json()
+    todos = requests.get(todos_url).json()
+
+    completed_todos = [todo for todo in todos if todo.get('completed') is True]
+
+    print('Employee {} is done with tasks({}/{}):'.format(user.get('name'),
+                                                          len(completed_todos),
+                                                          len(todos)))
+
+    for todo in completed_todos:
+        print('\t {}'.format(todo.get('title')))
