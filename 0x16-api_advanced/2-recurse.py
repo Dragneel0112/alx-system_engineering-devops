8e7afe718d6471fa3a9656bcac782b3aeb4f8c4f
Dragneel0112 <pillayruven@gmail.com>
2023-06-06 10:43:20 +0000

    Added variables count and next_page to function in 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,30 +4,35 @@ Queries Reddit Api
 '''
 import requests
 
-
-after = ""
-
-
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], next_page=None, count=0):
     '''
     Queries Reddit Api for recursively for titles
     Args:
         subreddit(str) - The name of the subreddit to check
+        hot_list - List to append
+        next_page - Page to be passed in parameter
+        count - Counter
     '''
-    global after
-    header = {"User-Agent": "Dragneel"}
-    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
-    if after:
-        url = url + "?after=" + after
-    r = requests.get(url, headers=header, allow_redirects=False)
+    user_agent = 'Dragneel'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    # if page specified, pass as parameter
+    if next_page:
+        url += '?after={}'.format(next_page)
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
     if r.status_code != 200:
         return None
-    after = r.json().get("after", "")
-    for i in r.json().get("data", None).get("children", None):
-        hot_list.append(i.get("data", None).get("title", None))
-    if after:
-        return recurse(subreddit, hot_list)
-    return hot_list
 
-if __name__ == "__main__":
-    pass
+    data = r.json()['data']
+    posts = data['children']
+    for post in posts:
+        count += 1
+        hot_list.append(post['data']['title'])
+
+    next_page = data['after']
+    if next_page is not None:
+        return recurse(subreddit, hot_list, next_page, count)
+    else:
+        return hot_list
